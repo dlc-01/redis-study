406019b02b42e1c71d1b661ad3bbbd2d396c18fd
DLC <shabrov23.02@gmail.com>
2025-12-18 11:49:01 +0300

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -22,6 +22,22 @@ func main() {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+	defer conn.Close()
 
-	conn.Write([]byte("+PONG\r\n"))
+	buffer := make([]byte, 1024)
+	for {
+		n, err := conn.Read(buffer)
+		if err != nil {
+			fmt.Println("Connection closed or error: ", err.Error())
+			break
+		}
+
+		fmt.Printf("Received %d bytes\n", n)
+
+		_, err = conn.Write([]byte("+PONG\r\n"))
+		if err != nil {
+			fmt.Println("Error writing response: ", err.Error())
+			break
+		}
+	}
 }
